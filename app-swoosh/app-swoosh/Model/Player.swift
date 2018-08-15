7b706f5c89584bed83e4a5f0e4d8fdc011faa585
Justin Rancourt <juramiche@Justins-MBP.fios-router.home>
2018-08-15 15:30:46 -0400

    created ibactions for player

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Justin Rancourt on 8/15/18.
+//  Copyright © 2018 Justin Rancourt. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
+
