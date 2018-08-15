d1a791492ecedc1a8c45345a804e317e5ffd61fe
Justin Rancourt <juramiche@Justins-MBP.fios-router.home>
2018-08-15 15:02:55 -0400

    added programatic segue

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -16,11 +16,9 @@ class WelcomeVC: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC(unwindSeque: UIStoryboardSegue){
+        
     }
-
-
+    
 }
 
